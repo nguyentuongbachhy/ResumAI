8251f922ef5e2c15cc748650288650262ac11048
nguyentuongbachhy <nguyentuongbachhy.phuong1@gmail.com>
2025-07-10 22:57:58 +0700

    chore: remove vit5-vietnews-qlora

--- train/vit5-vietnews-qlora/load_vitbase.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-model = AutoModelForSeq2SeqLM.from_pretrained("VietAI/vit5-base")
-tokenizer = AutoTokenizer.from_pretrained("VietAI/vit5-base", use_fast=False)
-
-model.save_pretrained("vit5-base")
-tokenizer.save_pretrained("vit5-base")
